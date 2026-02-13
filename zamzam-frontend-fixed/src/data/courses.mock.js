0a802d326533de1d4545d70175e2f1e92fcace9f
Lou-bna12 <loubnasellam49@gmail.com>
2026-02-13 16:42:36 +0100

    git commit

--- zamzam-frontend-fixed/src/data/courses.mock.js
+++ zamzam-frontend-fixed/src/data/courses.mock.js
@@ -11,20 +11,29 @@ export const products = [
     name: "Pommes",
     price: 250,
     category: "Fruits & Légumes",
-    image: "https://unsplash.com/fr/photos/un-tas-de-pommes-rouges-assises-les-unes-sur-les-autres-KcfMOET-py8"
+    image: "https://images.unsplash.com/photo-1567306226416-28f0efdc88ce",
   },
-  {
-    id: 2,
-    name: "Lait demi-écrémé",
-    price: 180,
-    category: "Produits frais",
-    image: "https://unsplash.com/fr/photos/mug-en-ceramique-blanche-sur-table-en-bois-marron-MMuOz3UNKeo",
-  },
+   {
+  id: 2,
+  name: "Fromage frais",
+  price: 180,
+  category: "Produits frais",
+  image: "https://plus.unsplash.com/premium_photo-1700145523789-764a456e6034?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZyb21hZ2V8ZW58MHx8MHx8fDA%3D",
+},
+
+
   {
     id: 3,
     name: "Riz long",
     price: 320,
     category: "Épicerie",
-    image: "https://unsplash.com/fr/photos/une-photo-dun-bol-en-bois-de-riz-non-cuit-place-sur-un-plateau-en-bois-assorti-xhs6vuE5oLY",
+    image: "https://images.unsplash.com/photo-1586201375761-83865001e31c",
+  },
+   {
+    id: 4,
+    name: "Jus dePommes",
+    price: 250,
+    category: "Boissons",
+    image: "https://images.unsplash.com/photo-1601055903647-ddf1ee9701b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGp1cyUyMGRlJTIwcG9tbWVzfGVufDB8fDB8fHww",
   },
 ];
